42cfeaa3829ff2178dd84b1b46052645f91f064d
Astrubale <nic04.simionato@gmail.com>
2024-05-27 15:23:44 +0200

    ModBlocks classs and baobab wood blocks

---
+++ src/main/java/com/astrubale/savanar/block/ModBlocks.java
@@ -0,0 +1,46 @@
+package com.astrubale.savanar.block;
+
+import com.astrubale.savanar.SavanaRewrite;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.minecraft.block.*;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
+public class ModBlocks {
+
+    public static final Block BAOBAB_LOG = registerBlock("baobab_log",
+            new PillarBlock(FabricBlockSettings.copyOf(Blocks.OAK_LOG).strength(4f)));
+    public static final Block BAOBAB_WOOD = registerBlock("baobab_wood",
+            new PillarBlock(FabricBlockSettings.copyOf(Blocks.OAK_WOOD).strength(4f)));
+    public static final Block STRIPPED_BAOBAB_LOG = registerBlock("stripped_baobab_log",
+            new PillarBlock(FabricBlockSettings.copyOf(Blocks.STRIPPED_OAK_LOG).strength(4f)));
+    public static final Block STRIPPED_BAOBAB_WOOD = registerBlock("stripped_baobab_wood",
+            new PillarBlock(FabricBlockSettings.copyOf(Blocks.STRIPPED_OAK_WOOD).strength(4f)));
+
+    public static final Block BAOBAB_PLANKS = registerBlock("baobab_planks",
+            new Block(FabricBlockSettings.copyOf(Blocks.OAK_PLANKS).strength(4f)));
+/*
+    public static final Block CHESTNUT_LEAVES = registerBlock("chestnut_leaves",
+            new LeavesBlock(FabricBlockSettings.copyOf(Blocks.OAK_LEAVES).strength(4f).nonOpaque()));
+
+    public static final Block CHESTNUT_SAPLING = registerBlock("chestnut_sapling",
+            new SaplingBlock(ModSaplingGenerators.CHESTNUT, FabricBlockSettings.copyOf(Blocks.OAK_SAPLING)));
+*/
+    private static Block registerBlock(String name, Block block) {
+        registerBlockItem(name, block);
+        return Registry.register(Registries.BLOCK, new Identifier(SavanaRewrite.MOD_ID, name), block);
+    }
+
+    private static Item registerBlockItem(String name, Block block) {
+        return Registry.register(Registries.ITEM, new Identifier(SavanaRewrite.MOD_ID, name),
+                new BlockItem(block, new FabricItemSettings()));
+    }
+
+    public static void registerModBlocks() {
+        SavanaRewrite.LOGGER.info("Registering ModBlocks for " + SavanaRewrite.MOD_ID);
+    }
+}
