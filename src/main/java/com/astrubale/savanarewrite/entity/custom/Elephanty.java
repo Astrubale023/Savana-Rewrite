cd673c2714ea44328b699305f09b4ff4877c8205
Astrubale <nic04.simionato@gmail.com>
2024-08-11 16:25:44 +0200

    herbivor class

---
+++ src/main/java/com/astrubale/savanarewrite/entity/custom/Elephanty.java
@@ -0,0 +1,7 @@
+package com.astrubale.savanarewrite.entity.custom;
+
+import net.minecraft.entity.passive.TameableEntity;
+import software.bernie.geckolib.animatable.GeoEntity;
+
+public class Elephanty extends TameableEntity implements GeoEntity {
+}
