b967addda17c53bed8b6f60b9f34228c399b0433
Astrubale <nic04.simionato@gmail.com>
2024-08-11 16:26:32 +0200

    elephanty animal

--- src/main/java/com/astrubale/savanarewrite/entity/custom/Elephanty.java
+++ src/main/java/com/astrubale/savanarewrite/entity/custom/Elephanty.java
@@ -1,7 +1,56 @@
 package com.astrubale.savanarewrite.entity.custom;
 
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.passive.PassiveEntity;
 import net.minecraft.entity.passive.TameableEntity;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.EntityView;
+import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
 import software.bernie.geckolib.animatable.GeoEntity;
+import software.bernie.geckolib.core.animatable.instance.AnimatableInstanceCache;
+import software.bernie.geckolib.core.animation.AnimatableManager;
 
-public class Elephanty extends TameableEntity implements GeoEntity {
+public class Elephanty extends TameableEntity implements GeoEntity, Herbivore {
+
+    protected Elephanty(EntityType<? extends TameableEntity> entityType, World world) {
+        super(entityType, world);
+    }
+
+    @Override
+    public boolean isNotBusy() {
+        return false;
+    }
+
+    @Override
+    public void setTargetPlant(BlockPos pos) {
+
+    }
+
+    @Override
+    public @Nullable BlockPos getTargetPlant() {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public PassiveEntity createChild(ServerWorld world, PassiveEntity entity) {
+        return null;
+    }
+
+    @Override
+    public EntityView method_48926() {
+        return null;
+    }
+
+    @Override
+    public void registerControllers(AnimatableManager.ControllerRegistrar controllers) {
+
+    }
+
+    @Override
+    public AnimatableInstanceCache getAnimatableInstanceCache() {
+        return null;
+    }
 }
